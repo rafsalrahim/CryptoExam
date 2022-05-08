af3614a0f6b78aef05eaaae8396f65090017b806
amalcs23 <amalcs2311@gmail.com>
2022-05-08 12:54:04 +0530

    Result calculation completed

--- backend/contracts/Student.sol
+++ backend/contracts/Student.sol
@@ -3,9 +3,34 @@ pragma solidity >=0.4.22 <0.9.0;
 
 interface Student {
 
+    struct StudentData{
+        address wallet;
+        string email;
+        bool feePaid;
+        uint[] ans;
+        bool attempt;
+        uint result;
+    }
+    
+    event registerStudentEvent(address _wallet,StudentData data);
+
     event feePaymentEvent(address from, uint amount);
 
+    event submitExamEvent(address wallet, uint[] ans);
+
+    event resultEvent(address wallet, uint result);
+
+    function registerStudent(
+        string memory _email)
+        external;
+
+    function getProfile() external view returns(StudentData memory);
+
     function feePayment() external payable;
+
+    function attendExam() external view returns(string memory);
+
+    function submitExam(uint[] memory _ans) external;
 
     
 }
--- backend/contracts/Validation.sol
+++ backend/contracts/Validation.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 /**
  * @dev Contract module which allows children to implement an emergency stop
  * mechanism that can be triggered by an authorized account.
@@ -12,6 +10,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * the functions of your contract. Note that they will not be pausable by
  * simply including this module, only once the modifiers are put in place.
  */
-abstract contract Security is Ownable{
-    
+abstract contract Validation {
+
+    modifier examEligible(bool _feePaid, bool _attend){
+        require(_feePaid == true, "Please complete the exam fees payment");
+        require(_attend == false, "You are already attended the exam");
+        _;
+    }
+
 }
