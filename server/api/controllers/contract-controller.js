d7f4ea02fc955fa554b7f766461f556342d148c8
amalcs23 <amalcs2311@gmail.com>
2022-05-21 18:20:10 +0530

    ropsten network added

--- server/api/controllers/contract-controller.js
+++ server/api/controllers/contract-controller.js
@@ -7,7 +7,7 @@ const DEPLOY = async (req, res, next) =>{
         let q_hash = "myexamhashvaluehere"; //need to replace
         let ans_key = [1,2]; // need to change. num_qus = length of ans_key
         let num_qus = 2;
-        let fee = "2"; // fee in ether
+        let fee = "0000000000000001"; // fee in ether
         result = await contractModel.DEPLOY(num_qus, q_hash, ans_key, fee);
         res.status(200).json(result);
     }catch(err){
